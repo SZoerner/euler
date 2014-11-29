b5e1435681615cafcebd9ae78453de7842eb7582
Sebastian Zoerner <zoerner7@gmail.com>
2014-11-29 11:59:13 +0100

    Problem 31

--- src/euler/euler021.clj
+++ src/euler/euler021.clj
@@ -278,3 +278,24 @@
                (= n)))]
     (reduce + (filter powers?
                       (range 2 (Math/pow 10 (inc exp))))))) ; not really sure what the upper limit is..
+
+
+; Problem 31 - Coin sums
+; -----------------------
+; In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+;
+; 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+; It is possible to make £2 in the following way:
+;
+; 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+; How many different ways can £2 be made using any number of coins?
+
+(defn p031 [n]
+  (let [step
+        (fn step [n coins]
+          (cond (= n 0) 1                                   ; valid combination
+                (< n 0) 0                                   ; no valid combination
+                :else (if (< n (first coins))               ; first item is bigger than n
+                        (step n (rest coins))
+                        (reduce + (map #(step n %) coins)))))]
+    (step n '(200 100 50 20 10 5 2 1))))
