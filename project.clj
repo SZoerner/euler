1daabb78d25c9416f1a9140ecc21bc3be4d668cb
Sebastian Zoerner <zoerner7@gmail.com>
2015-01-30 17:37:00 +0100

    first midje tests, removed IntelliJ files, removed .gitignore

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/SZoerner/euler"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.8"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [org.clojure/test.check "0.7.0"]]}})
