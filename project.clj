16ad60e7caf6f1dc97d6ac9bd9c4dec21419092c
Sebastian Zoerner <zoerner7@gmail.com>
2015-01-31 16:39:17 +0100

    travis midje support

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [org.clojure/test.check "0.7.0"]]}})
+                                  [org.clojure/test.check "0.7.0"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
