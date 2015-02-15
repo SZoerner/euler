e2486180c94045e640f048d19440e500bae96fa7
Sebastian Zoerner <zoerner7@gmail.com>
2015-02-15 16:27:29 +0100

    p027

--- test/euler/euler021_test.clj
+++ test/euler/euler021_test.clj
@@ -5,8 +5,8 @@
 
 (deftest euler021-tests
   (fact "Problem 21"
-     ; (p021) => 31626
-     ) ;; too slow - makes cloverage abort
+     ; (p021) => 31626  ;; too slow - makes cloverage abort
+) 
   
   (fact "Problem 22"
         (p022) => 871198282)
@@ -17,17 +17,18 @@
   (fact "Problem 24"
         (p024) => '(2 7 8 3 9 1 5 4 6 0))
 
-   (fact "Problem 25" ;; too slow - makes cloverage abort
-       (p025) => 4782)                            ; n² + n + 41
+  (fact "Problem 25" ;; too slow - makes cloverage abort
+        (p025) => 4782)                            ; n² + n + 41
   
   (fact "Problem 26"
-      (rec-cycle 1 6) => '(6)
-      (rec-cycle 1 7) => '(1 4 2 8 5 7)
-      (p026) => 983)
-  ; (fact "Problem 27" ;; too slow - makes cloverage abort
-  ;     (consec-primes 1 41) => 40                       ; n² + n + 41
-  ;     (consec-primes -79 1601) => 80                    ; n² − 79n + 1601
-  ;     (p027) => -59231)                              ; spoiler alert!
+        (rec-cycle 1 6) => '(6)
+        (rec-cycle 1 7) => '(1 4 2 8 5 7)
+        (p026) => 983)
+  
+  (fact "Problem 27" ;; too slow - makes cloverage abort
+        (consec-primes 1 41) => 40                       ; n² + n + 41
+        (consec-primes -79 1601) => 80                    ; n² − 79n + 1601
+        (p027) => -59231)                              ; spoiler alert!
   
   (fact "Problem 28"
         (p028 3) => 25
