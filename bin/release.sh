2d1d044beb690541bdabc01122837b23c289ee74
Sebastian Zoerner <zoerner7@gmail.com>
2015-02-15 17:12:17 +0100

    new documentation push.

--- bin/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Script to push a release with lein-release and then push docs.
-# https://github.com/Prismatic/plumbing/tree/master/bin
-cd `dirname $0`
-lein release
-./push_docs.sh
