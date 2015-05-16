d81c5dde083d106aed72c8799ac3cab1cfe79118
Sebastian Zoerner <zoerner7@gmail.com>
2015-05-16 11:45:33 +0200

    upgrade to cloverage 1.0.4 SNAPSHOT

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.4' lein2 with-profile +coverage cloverage -o cov --coveralls
+CLOVERAGE_VERSION='1.0.4-SNAPSHOT' lein2 with-profile +coverage cloverage -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
