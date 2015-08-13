46ae8b2158b1ab6ea7edc3cdd7210c253a91fabc
Sebastian Zoerner <zoerner7@gmail.com>
2015-08-13 16:31:47 +0200

    Cloverage 1.0.7-SNAPSHOT

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.6' lein2 with-profile +coverage cloverage -o cov --coveralls --codecov
+CLOVERAGE_VERSION='1.0.7-SNAPSHOT' lein2 with-profile +coverage cloverage -o cov --coveralls --codecov
 #curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
 curl -s https://codecov.io/bash
