16175c2f9f9ef9dc7baf6e3a77c889e859adcb60
Sebastian Zoerner <zoerner7@gmail.com>
2015-08-13 16:26:12 +0200

    Cloverage after success.

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -2,4 +2,5 @@
 
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 CLOVERAGE_VERSION='1.0.6' lein2 with-profile +coverage cloverage -o cov --coveralls --codecov
-curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
+#curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
+curl -s https://codecov.io/bash
