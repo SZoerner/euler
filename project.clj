1c52e56d1b1eb4de3f1ab8c0ae5d3b910d10bb05
Sebastian Zoerner <zoerner7@gmail.com>
2015-10-17 11:10:10 +0200

    add lein test-out

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                                   [org.clojure/test.check "0.8.2"]]
                    :injections [(require 'flare.clojure-test)
                                 (flare.clojure-test/install!)]}
-             :coverage {:plugins [[lein-cloverage "1.0.6"]]}}
+             :coverage {:plugins [[lein-cloverage "1.0.6"]
+                                  [lein-test-out "0.3.1" :exclusions [org.clojure/clojure]]]}}
   :lein-release {:deploy-via :clojars}
   :marginalia {:css ["marg.css"]
                :javascript ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"]
