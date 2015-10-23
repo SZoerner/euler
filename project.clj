74b6055e2a6fc7b95046ba13cc9a4be110681d1e
Sebastian Zoerner <zoerner7@gmail.com>
2015-10-23 22:19:24 +0200

    Moved midje out of dev dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/SZoerner/euler"
   :license {:name "MIT License"
             :url  "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [midje "1.7.0"]
+                 [org.clojure/test.check "0.8.2"]]
   :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]
             [lein-test-out "0.3.1" :exclusions [org.clojure/clojure]]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]
-                                  [org.clojure/test.check "0.8.2"]]
-                   :injections [(require 'flare.clojure-test)
+  :profiles {:dev {:injections [(require 'flare.clojure-test)
                                 (flare.clojure-test/install!)]}}
   :lein-release {:deploy-via :clojars}
   :marginalia {:css ["marg.css"]
