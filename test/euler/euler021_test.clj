005d752e0c299f9832872b246fa768213439968b
Sebastian Zoerner <zoerner7@gmail.com>
2016-03-04 16:50:29 +0100

    p021: moving tests form midje back to clj.test

--- test/euler/euler021_test.clj
+++ test/euler/euler021_test.clj
@@ -1,45 +1,43 @@
 (ns euler.euler021-test
-  (:require [clojure.test :refer [deftest]]
-            [euler.euler021 :as e021]
-            [midje.sweet :refer [fact]]))
+  (:require [clojure.test :refer [deftest is]]
+            [euler.euler021 :as e021]))
 
-(deftest euler021-tests
-  (fact "Problem 21"
-        (e021/p021) => 31626)
+(deftest problem-021
+  (is (= (e021/p021) 31626)))
 
-  (fact "Problem 22"
-        (e021/p022) => 871198282)
+(deftest problem-022
+  (is (= (e021/p022) 871198282)))
 
-  (fact "Problem 23"
-        (e021/p023 [12]) => 12
-        (e021/p023 (range 25)) => 276
-        (e021/p023) => 4179871 :slow)
+(deftest problem-023
+  (is (= (e021/p023 [12]) 12))
+  (is (= (e021/p023 (range 25)) 276))
+  (is (= (e021/p023) 4179871)))
 
-  (fact "Problem 24"
-        (e021/p024) => '(2 7 8 3 9 1 5 4 6 0))
+(deftest problem-024
+  (is (= (e021/p024) '(2 7 8 3 9 1 5 4 6 0))))
 
-  (fact "Problem 25"
-        (e021/p025) => 4782)
+(deftest problem-025
+  (is (= (e021/p025) 4782)))
 
-  (fact "Problem 26"
-        (e021/rec-cycle 1 6) => '(6)
-        (e021/rec-cycle 1 7) => '(1 4 2 8 5 7)
-        (e021/p026) => 983)
+(deftest problem-026
+  (is (= (e021/rec-cycle 1 6) '(6)))
+  (is (= (e021/rec-cycle 1 7) '(1 4 2 8 5 7)))
+  (is (= (e021/p026) 983)))
 
-  (fact "Problem 27"
-        (e021/consec-primes 1 41) => 40                       ; n² + n + 41
-        (e021/consec-primes -79 1601) => 80                    ; n² − 79n + 1601
-        (e021/p027) => -59231 :slow)
+(deftest problem-027
+  (is (= (e021/consec-primes 1 41) 40))           ; n² + n + 41
+  (is (= (e021/consec-primes -79 1601) 80))       ; n² − 79n + 1601
+  (is (= (e021/p027) -59231)))
 
-  (fact "Problem 28"
-        (e021/p028 3) => 25
-        (e021/p028 5) => 101
-        (e021/p028) => 669171001)
+(deftest problem-028
+  (is (= (e021/p028 3) 25))
+  (is (= (e021/p028 5) 101))
+  (is (= (e021/p028) 669171001)))
 
-  (fact "Problem 29"
-        (e021/p029 5) => 15
-        (e021/p029) => 9183)
+(deftest problem-029
+  (is (= (e021/p029 5) 15))
+  (is (= (e021/p029) 9183)))
 
-  (fact "Problem 30" :slow
-        (e021/p030 4) => 19316
-        (e021/p030) => 443839))
+(deftest problem-030
+  (is (= (e021/p030 4) 19316))
+  (is (= (e021/p030) 443839)))
