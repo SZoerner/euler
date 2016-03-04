505ebd5f44520ff4c8521d182b6800506163367f
Sebastian Zoerner <zoerner7@gmail.com>
2016-03-04 16:53:30 +0100

    p031: moving tests form midje back to clj.test

--- test/euler/euler031_test.clj
+++ test/euler/euler031_test.clj
@@ -1,23 +1,27 @@
 (ns euler.euler031-test
-  (:require [clojure.test :refer [deftest]]
-            [euler.euler031 :as e031]
-            [midje.sweet :refer [fact]]))
+  (:require [clojure.test :refer [deftest is]]
+            [euler.euler031 :as e031]))
 
-(deftest euler031-tests
-  (fact "Problem 31"
-        (e031/p031 1 [1]) => 1
-        (e031/p031 3 [1 2]) => 2
-        (e031/p031 5 [1 2]) => 3
-        (e031/p031) => 73682)
-  (fact "Problem 32"
-        (e031/p032) => 45228)
-  (fact "Problem 33"
-        (e031/p033) => 100)
-  (fact "Problem 34"
-        (e031/p034) => 40730)
-  (fact "Problem 35"
-        (e031/p035) => 55)
-  (fact "Problem 36"
-        (e031/p036) => 872187)
-  (fact "Problem 37"
-        (e031/p037) => 748317))
+(deftest problem-031
+  (is (= (e031/p031 1 [1]) 1))
+  (is (= (e031/p031 3 [1 2]) 2))
+  (is (= (e031/p031 5 [1 2]) 3))
+  (is (= (e031/p031) 73682)))
+
+(deftest problem-032
+  (is (= (e031/p032) 45228)))
+
+(deftest problem-033
+  (is (= (e031/p033) 100)))
+
+(deftest problem-034
+  (is (= (e031/p034) 40730)))
+
+(deftest problem-035
+  (is (= (e031/p035) 55)))
+
+(deftest problem-036
+  (is (= (e031/p036) 872187)))
+
+(deftest problem-037
+  (is (= (e031/p037) 748317)))
