eda791401d7cf0d83a7146e4a05ea6f6bd44d9fe
Sebastian Zoerner <zoerner7@gmail.com>
2016-03-04 14:51:19 +0100

    removed plugins

--- project.clj
+++ project.clj
@@ -4,14 +4,9 @@
   :license {:name "MIT License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [midje "1.8.3"]
                  [org.clojure/test.check "0.9.0"]]
-  :dev-dependencies [[midje "1.8.3"  :exclusions [org.clojure/clojure]]
-                     [lein-midje "3.2"]]
-  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]
-            [lein-test-out "0.3.1" :exclusions [org.clojure/clojure]]]
-  :profiles {:dev {:injections [(require 'flare.clojure-test)
-                                (flare.clojure-test/install!)]}}
-  :lein-release {:deploy-via :clojars}
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]]
   :marginalia {:css ["marg.css"]
                :javascript ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"]
                :dir ["doc"]}
