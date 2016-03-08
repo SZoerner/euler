ef399c86601ca84e800b7887ea3f3c20b2794b61
Sebastian Zoerner <zoerner7@gmail.com>
2016-03-08 08:14:04 +0100

    re-enable coveralls and codecov support

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.7-SNAPSHOT' lein2 with-profile +coverage cloverage -o cov --coveralls --codecov
-#curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
+CLOVERAGE_VERSION='1.0.7-SNAPSHOT' lein2 cloverage -o cov --coveralls --codecov
+curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
 curl -s https://codecov.io/bash
